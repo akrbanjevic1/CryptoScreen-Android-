2091e9ccc8860dae1e8c0d5220ff473feb6a7f97
akrbanjevic1 <akrbanjevic1@luc.edu>
2019-07-04 18:52:16 -0500

    First commit. Created a button, intent, and 2 activities. The second activity has a textview with a message to greet the user. The goal is to display crypto information here.

---
+++ app/src/main/java/com/aknu/cryptoscreen/MainActivity.java
@@ -0,0 +1,29 @@
+package com.aknu.cryptoscreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.CryptoScreen.CRYPTOSCREEN";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        //Here I have created and intent. Clicking the button on the mainactivity will allow for the
+        //new activity screen to open. This screen will be used to display crypto information.
+        Intent intent = new Intent(this, CryptoActivity.class);
+        //I am instantiating a String variable (message), and then setting it to hold a value.
+        String message;
+        message = "This is how the Cryptos are looking today: ";
+        //Here, I am having the intent carry this message over to the CryptoActivity Activity.
+        intent.putExtra(EXTRA_MESSAGE, message);
+        //This line starts the next activity upon button click.
+        startActivity(intent);
+
+    }
+}
