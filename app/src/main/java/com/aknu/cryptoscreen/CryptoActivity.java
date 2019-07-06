a711203b341d7fbc3b6931227cfb10711840e012
akrbanjevic1 <akrbanjevic1@luc.edu>
2019-07-06 13:42:56 -0500

    First commit. Created a button, intent, and 2 activities. The second activity has a textview with a message to greet the user. The goal is to display crypto information here.

--- app/src/main/java/com/aknu/cryptoscreen/CryptoActivity.java
+++ app/src/main/java/com/aknu/cryptoscreen/CryptoActivity.java
@@ -3,6 +3,7 @@ package com.aknu.cryptoscreen;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CryptoActivity extends AppCompatActivity {
@@ -18,7 +19,16 @@ public class CryptoActivity extends AppCompatActivity {
         //as the value for the textview we instantiated.
         TextView textView = findViewById(R.id.Message);
         textView.setText(message);
+        //Here, we are instantiating the textviews for the various cryptos, and we are setting their
+        //text to be the parsed information.
+        TextView BitCoin = findViewById(R.id.BitCoin);
+
+        TextView Ethereum = findViewById(R.id.EthereumCoin);
+
+        TextView LiteCoin = findViewById(R.id.LiteCoin);
 
         //HERE, you wanted to create a screen that portrays some popular cryptos information.
+
+
     }
 }
