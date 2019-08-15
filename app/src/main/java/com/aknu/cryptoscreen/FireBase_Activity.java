554962d676f532b0359e2280cd304bdfed9bb3ba
akrbanjevic1 <akrbanjevic1@luc.edu>
2019-08-15 17:26:43 -0500

    Started process of creating e-mail login via Firebase. Added necessary views to the Firebase activity. Attempting to create logic that checks if user is already signed-in.

--- app/src/main/java/com/aknu/cryptoscreen/FireBase_Activity.java
+++ app/src/main/java/com/aknu/cryptoscreen/FireBase_Activity.java
@@ -1,16 +1,90 @@
 package com.aknu.cryptoscreen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class FireBase_Activity extends AppCompatActivity {
 
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    private Button mLoginBtn;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_base_);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        if(!FirebaseApp.getApps(this).isEmpty()) {
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        }
+        GoogleSignInClient mGoogleSignInClient;
         //In this activity, I want to integrate Firebase so that the user can log-in and then
         //store and view the data on the cryptocurrencies that they hold.
+
+        mEmailField = (EditText) findViewById(R.id.emailField);
+        mPasswordField = (EditText) findViewById(R.id.passwordField);
+
+        mLoginBtn = (Button) findViewById(R.id.loginButton);
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignIn();
+            }
+        });
+    }
+
+    private void startSignIn() {
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(FireBase_Activity.this, "Sign In Problem", Toast.LENGTH_LONG).show();
+        }
+
+        else{
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()) {
+                        Toast.makeText(FireBase_Activity.this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+
+        }
+
     }
 }
